0e721ad3f504b95551e8eb13e158a172730bc077
Subrat Behera <subratkumar0807@gmail.com>
2020-02-28 14:30:06 +0530

    Changed the IP address

--- GreenDeck_App.py
+++ GreenDeck_App.py
@@ -273,4 +273,4 @@ if __name__ == '__main__':
     prepare_dataset('dumps/netaporter_gb.json')
     
     # RUNNNING FLASK APP
-    app.run(debug=False, host = '127.0.0.1', port=5000)
+    app.run(debug=False, host = '0.0.0.0', port=5000)
